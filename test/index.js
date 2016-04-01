f4e89c874dfee4a937a9156af237f7cf9b185dc8
Eran Hammer <eran@hammer.io>
2016-04-01 12:37:37 -0700

    Add non 500 error payloads. Closes #1

--- test/index.js
+++ test/index.js
@@ -24,7 +24,7 @@ const expect = Code.expect;
 
 describe('Bananas', () => {
 
-    it('logs events', { parallel: false }, (done) => {
+    it('logs error event', { parallel: false }, (done) => {
 
         const server = new Hapi.Server({ debug: false });
         server.connection();
@@ -65,6 +65,58 @@ describe('Bananas', () => {
 
                         const messages = updates[0].options.payload.split('\n');
                         expect(messages.length).to.equal(3);
+                        Wreck.post = orig;
+
+                        server.stop((err) => {
+
+                            expect(err).to.not.exist();
+                            done();
+                        });
+                    }, 110);
+                });
+            });
+        });
+    });
+
+    it('logs valid event', { parallel: false }, (done) => {
+
+        const server = new Hapi.Server({ debug: false });
+        server.connection();
+
+        const settings = {
+            token: 'abcdefg',
+            intervalMsec: 50
+        };
+
+        server.register({ register: Bananas, options: settings }, (err) => {
+
+            expect(err).to.not.exist();
+
+            server.route({
+                path: '/',
+                method: 'GET',
+                handler: function (request, reply) {
+
+                    return reply('hello');
+                }
+            });
+
+            const updates = [];
+            const orig = Wreck.post;
+            Wreck.post = function (uri, options, ignore) {
+
+                updates.push({ uri, options });
+            };
+
+            server.start((err) => {
+
+                expect(err).to.not.exist();
+
+                server.inject('/', (res) => {
+
+                    setTimeout(() => {
+
+                        expect(updates.length).to.equal(1);
                         Wreck.post = orig;
 
                         server.stop((err) => {
